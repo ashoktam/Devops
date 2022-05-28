c1f5a0bc30d4110206736c82c5252aec1de4dd61
Chyangbadai <chyangba69@gmail.com>
2022-05-28 16:51:52 +0000

    updated new

--- ash.java
+++ ash.java
@@ -1,5 +1,7 @@
-This is java code for demo
-public class Multiply {
+i want to merge again 
+whatsu up  guys 
+This is java code for demo purpose only
+ ipublic class Multiply {
 
 	public static void main(String[] args) {
 		int a = 5;
